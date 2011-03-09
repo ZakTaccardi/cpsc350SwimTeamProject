17bbfc3bb5c80e8958e8479660d48683c86152a2
= <ztaccardi@gmail.com>
2011-03-09 11:20:33 -0500

    added an incomplete siteNavigator file to replace the header

---
+++ siteNavigator.php
@@ -0,0 +1,19 @@
+<body id="page" onload="initULMenu('menu_main')"> 
+<div id="header" class="canvas"> 
+  <div id="headerTransparent"> 
+    <div id="logo"><a href="index.php?src="><h1>Stingrays Swim Team</h1></a></div><!-- end logo --> 
+    <div id="headerMenu"></div> 
+  </div> 
+</div> 
+
+<div id="content" class="canvas"> 
+ <div id="contentTop"> 
+ <div id="contentBot"> 
+  <div class="left"> 
+    <div id="menu_main"> 
+  <ul class="menu_main"> 
+	 <li class="item3"><a class="first" href="Home.php" >Home</a></li> 
+     <li class="item1"><a  href="login.php" >Login</a></li> 
+     <li class="item2"><a  href="logout.php" >Logout</a></li> 
+  </ul> 
+</div> 

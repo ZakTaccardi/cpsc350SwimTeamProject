b8d21d041674144a53a10f038f0bd891d72d27c5
jhouchen <Jonathan@.(none)>
2011-03-09 11:17:50 -0500

    added admin functionality to login

--- loginController.php
+++ loginController.php
@@ -17,19 +17,27 @@ session_start();
   $password = $_POST['password'];
 
    $query = "select * from families WHERE email = '$email' AND password = SHA('$password')";
+      
    $result = mysqli_query($db, $query);
-   
    if ($row = mysqli_fetch_array($result))
    {
-		 //Begin a session and create a session variable in
+     	//Begin a session and create a session variable in
 		//the $_SESSION array.
-		 $_SESSION['currentuser'] = $usersName;
+		$_SESSION['currentuser'] = $usersName;
 		echo("<p>You are logged in as ");
 		
 		echo $_SESSION['currentuser'];
 		
 		echo("</p>");
    		echo "<p><a href=\"home.php\">Continue</a></p>";	
+		//Handle if admin
+		if($row['accessLevel'] = 3){
+		 $_SESSION['accessLevel'] = 3;
+		}else if($row['accessLevel'] = 2){
+		 $_SESSION['accessLevel'] = 2;
+	    }else{
+		 $_SESSION['accessLevel'] = 1;
+	    }
 		$_SESSION['badLogin'] = 0;
    }
    else

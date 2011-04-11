ee9ea3fd63de9170cb0933f9dfa0238daf6c6b32
Patrick <pconnell89@gmail.com>
2011-04-10 20:55:35 -0400

    orderComplete.php now displays all previous orders

--- orderComplete.php
+++ orderComplete.php
@@ -1,21 +1,51 @@
 <?php 
 session_start();
-ob_start();
+include('header.php');
+?>
+<html>
+<body>
+<?php
+include('siteNavigator.php');
 ?>
 
+<!-- Page info goes below-->
 <!--orderComplete.php 
 need to further include:
 	viewing older orders
 -->
 
+<h1>Order Form History</h1>
+<h2>View your transaction history</h2>
+<table>
+	<tr>
+		<th>Date</th>
+		<th>Total Cost</th>
+		<th>Download</th>
+	</tr>
 <?php
 	include "DB.php";
 	$userID = $_SESSION['familyID'];
-	$query = "SELECT orderID FROM swimteam.order WHERE familyID = '$userID' ORDER BY orderID DESC LIMIT 1;";
-	$result = mysqli_query($db,$query);
+	$recentOrderQuery = "SELECT * FROM swimteam.order WHERE familyID = '$userID' ORDER BY orderID DESC;";
+	$result = mysqli_query($db,$recentOrderQuery);
+	$name = $_SESSION['userLastName'];
 	while ($row = mysqli_fetch_array($result)){
 		$orderID = $row['orderID'];
+		$datePlaced = $row['datePlaced'];
+		$total = $row['totalPaid'];
+		echo "<tr>
+				<td>$datePlaced</td>
+				<td>$total</td>
+				<td>
+					<a href ='download.php?download_file=$name$orderID.csv'>$name$orderID.csv</a>
+				</td>
+			  </tr>";
 	}
-	$name = $_SESSION['userLastName'];
-	echo "<a href ='download.php?download_file=$name$orderID.csv'>Download here</a>"
 ?>
+</table>
+
+<!-- Page info goes above-->
+<?php
+include('footer.php');
+?>
+</body>
+</html>

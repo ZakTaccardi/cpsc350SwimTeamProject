fd6b2bc03bee7fbfcf7abdbf50a1b7e3cd6197b5
Patrick <pconnell89@gmail.com>
2011-04-10 22:08:35 -0400

    modified editGiftCard.php to allow searching for gift cards by vendor id and vendor name

---
+++ editGiftCard.php
@@ -0,0 +1,92 @@
+<?php 
+session_start();
+include('header.php');
+?>
+
+
+<html>
+<body>
+<?php
+include('siteNavigator.php');
+?>
+
+
+<h1>Manage Gift Cards</h1>
+<form action = "editGiftCard.php" method = "POST">
+<table>
+
+	<tr>
+		<td>Search for gift cards</td>
+		<td><input type = "text" name = "searchtext"></td>
+	</tr>
+	<tr>
+		<td>Look up by vendor ID</td>
+		<td><input type = "radio" name = "searchtype" value = 'vendorID'/></td>
+	</tr>
+	<tr>
+		<td>Look up by vendor name</td>
+		<td><input type = "radio" name = "searchtype" value = 'vendorName'/></td>
+	</tr>
+	<tr><td><br/>
+		<input type = "submit" value = "Search" id="submit" />
+	</td></tr>
+	<!--<tr><td></td></tr> -->
+</table>
+</form>
+
+<?php
+	//we can only display this page if user is admin.
+	include "DB.php";
+	$searchtype = $_POST['searchtype'];
+	if ($searchtype != ''){
+		echo "
+			<table>
+			<tr>
+				<th>Name</th>
+				<th>Cost</th>
+				<th>Percent</th>
+				<th>Available?</th>
+			</tr>";
+		$query = "SELECT vendor, cost, percent, isAvailable FROM giftcards WHERE ";
+		if ($searchtype == 'vendorID'){
+			$vendorIDtext = $_POST['searchtext'];
+			$sanitizedText = mysqli_real_escape_string($db, $vendorIDtext);
+			$query = $query."orderFormCardID = $sanitizedText;";
+		}else if ($searchtype == 'vendorName'){
+			$vendorName = $_POST['searchtext'];
+			$sanitizedText = mysqli_real_escape_string($db, $vendorName);
+			$query = $query."vendor LIKE '%$sanitizedText%';";	
+		}
+	
+		$result = mysqli_query($db, $query);
+		while ($row = mysqli_fetch_array($result)){
+			$vendor = $row['vendor'];
+			$cost = "$".$row['cost'].".00";
+			$percent = $row['percent']."%";
+			if ($row['isAvailable'] == 1){
+				$isAvailable = 'yes';
+			}else{
+				$isAvailable = 'no';
+			}
+			echo "
+				<tr>
+					<td >$vendor</td>
+					<td>$cost</td>
+					<td>$percent</td>
+					<td>$isAvailable</td>
+					<td><input type = 'submit' value = 'edit'/></td>
+				</tr>";
+		}
+
+		echo "</table>";
+	}
+?>
+
+
+
+<!-- Page info goes above-->
+<?php
+include('footer.php');
+?>
+</body>
+</html>
